7b8fe287810debd0a1a8fab3c2e48625240d1b52
Shirley <shirleyzhu@college.harvard.edu>
2025-07-09 11:10:42 -0500

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by ZhuMacPro on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
